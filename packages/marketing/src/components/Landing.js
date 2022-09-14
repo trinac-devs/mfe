8416d6f7b7cdc6fe3e8dbaf823a070b17fcb85df
traxx10 <codetraxx10@gmail.com>
2022-09-14 07:13:13 +0100

    added fix for container

--- packages/marketing/src/components/Landing.js
+++ packages/marketing/src/components/Landing.js
@@ -33,10 +33,12 @@ const useStyles = makeStyles((theme) => ({
   icon: {
     marginRight: theme.spacing(2),
   },
+
   heroContent: {
     backgroundColor: theme.palette.background.paper,
     padding: theme.spacing(8, 0, 6),
   },
+
   heroButtons: {
     marginTop: theme.spacing(4),
   },
