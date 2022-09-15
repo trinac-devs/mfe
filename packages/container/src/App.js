05b7f5dee8c53d2e9085e93f24fc233f0fe0d844
traxx10 <codetraxx10@gmail.com>
2022-09-15 09:40:53 +0100

    finished navigation

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { createGenerateClassName, StylesProvider } from '@material-ui/core';
 
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
-import { createGenerateClassName, StylesProvider } from '@material-ui/core';
 
 const generateClassName = createGenerateClassName({
   productionPrefix: 'co',
--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history';
+
 import App from './App';
 
 // Mount function to start up app
-const mount = (el) => {
-  ReactDOM.render(<App />, el);
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
+
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+
+  ReactDOM.render(<App history={history} />, el);
+
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      const { pathname } = history.location;
+      if (pathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+    },
+  };
 };
 
 // If we are in development in isolation, call mount immediately
@@ -12,7 +29,9 @@ if (process.env.NODE_ENV === 'development') {
   const devRoot = document.querySelector('#_marketing-dev-root');
 
   if (devRoot) {
-    mount(devRoot);
+    mount(devRoot, {
+      defaultHistory: createBrowserHistory(),
+    });
   }
 }
 
